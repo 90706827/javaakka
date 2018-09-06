973f61ad02004da361eb87512ad00160d1df088a
Mr.Jangni <90706827@163.com>
2018-09-06 17:34:28 +0800

    actor

--- src/main/java/com/jangni/javaakka/helloword/HelloWorld.java
+++ src/main/java/com/jangni/javaakka/helloword/HelloWorld.java
@@ -1,8 +1,11 @@
 package com.jangni.javaakka.helloword;
 
+import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
 /**
  * @program: javaakka
@@ -11,22 +14,32 @@ import akka.actor.UntypedActor;
  * @create: 2018-06-29 00:02
  **/
 
-public class HelloWorld extends UntypedActor {
+public class HelloWorld extends AbstractActor {
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
 
     @Override
-    public void preStart() {
-        // create the greeter actor
-        final ActorRef greeter = getContext().actorOf(Props.create(Greeter.class), "greeter");
-        // tell it to perform the greeting
-        greeter.tell(Greeter.Msg.GREET, getSelf());
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals("abc",a ->{
+                    log.info("Received String message abc:{}", a);
+                })
+                .match(String.class, s -> {
+                    log.info("Received String message:{}", s);
+                    getSender().tell(s, getSelf());
+                })
+                .match(Integer.class, i -> {
+                    log.info("Received String message:{}", i);
+                    getSender().tell(i + 2, getSelf());
+                })
+                .matchAny(o -> log.info("received unknown message"))
+                .build();
     }
 
-    @Override
-    public void onReceive(Object msg) throws Exception {
-        if (msg == Greeter.Msg.DONE) {
-            // when the greeter is done, stop this actor and with it the application
-            getContext().stop(getSelf());
-        } else
-            unhandled(msg);
+    public static void main(String[] args){
+        ActorSystem system = ActorSystem.create("SwapperSystem");
+        ActorRef helloActor = system.actorOf(Props.create(HelloWorld.class));
+        helloActor.tell("abcd",ActorRef.noSender());
+
     }
+
 }
